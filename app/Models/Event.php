f1122a5aa5d41c25b73b7fa2a3ca0dccb24648c3
Joel <iouubns10@gmail.com>
2024-09-11 17:17:43 +0800

    11/9/2024 2

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'event_id';
+
+    // Specify the attributes that are mass assignable
+    protected $fillable = [
+        'cafe_id',
+        'event_name',
+        'event_description',
+        'event_date',
+        'event_time',
+    ];
+
+    // Specify the attributes that should be cast to native types
+    protected $casts = [
+        'event_date' => 'date',
+        'event_time' => 'datetime',
+    ];
+
+    // Define relationships
+    public function cafe()
+    {
+        return $this->belongsTo(Cafe::class, 'cafe_id');
+    }
 }
